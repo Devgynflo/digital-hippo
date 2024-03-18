7d4bdf7a5238d37e4987ff231c3e55fc3f8b8276
gynflo <ginet.florian.dev@gmail.com>
2024-03-18 15:02:56 +0100

    Feat: Create thank-you page with order status

---
+++ src/app/_components/payment-status.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+import { NextPage } from "next";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+interface PaymentStatusProps {
+  orderEmail: string;
+  orderId: string;
+  isPaid: boolean;
+}
+
+export const PaymentStatus: NextPage<PaymentStatusProps> = ({
+  orderEmail,
+  orderId,
+  isPaid,
+}) => {
+  const router = useRouter();
+  const { data } = trpc.paymentRouter.pollOrderStatus.useQuery(
+    { orderId },
+    {
+      enabled: isPaid === false,
+      refetchInterval: (data) => (data?.isPaid ? false : 1000),
+    },
+  );
+
+  useEffect(() => {
+    if (data?.isPaid) router.refresh();
+  }, [data?.isPaid, router]);
+
+  return (
+    <div className="gridcols-2 tex-gray-600 mt-16 grid gap-x-4 text-sm">
+      <div>
+        <p className="font-medium text-gray-900">Shipping to</p>
+        <p>{orderEmail}</p>
+      </div>
+
+      <div>
+        <p className="font-medium text-gray-900">
+          {isPaid ? "Payment successful" : "Pending payment"}
+        </p>
+      </div>
+    </div>
+  );
+};
